1f8c42ade55d7a7c1f618595d5fdfd7cab318ce0
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2024-03-02 11:23:05 +0300

    fix home page

--- src/components/home/HomeNav.jsx
+++ src/components/home/HomeNav.jsx
@@ -3,27 +3,28 @@ import { Link } from 'react-router-dom'
 
 function HomeNav() {
   return (
-    <div className='hidden md:block'>
+    <div className=''>
          <div className='absolute  w-full z-10 '>
-        <div className='mt-8 md:ml-10 ml-5 flex'>
+        <div className='md:mt-8 mt-4 md:ml-24 ml-5 flex'>
           <Link href={'/'}>
             <div className='flex'>
                 <img
-                src={"/favicon.ico"}
+                src={"/logom.png"}
                 alt='Logo'
+                className='w-[60px] '
                 />
-                <p className='  mt-2 md:ml-1 ml-3 text-xl font-bold  '>Moodly</p>
+                <p className='mt-[16px] md:mt-2 md:ml-1 ml-3 md:text-xl text-2xl  font-bold  '>Moodly</p>
             </div>
             </Link>
 
-            <button className='mt-2 absolute md:right-[250px] right-10 flex bg-[#FF4967] rounded-full p-[2px] w-[120px] h-[50px] px-8 py-3' >
+            <button className='mt-2 absolute md:right-[250px] right-5 flex bg-[#FF4967] rounded-full p-[2px] md:w-[120px] h-[50px] px-8 py-3' >
 
                 <p className=' text-white text-center mx-auto font-medium'>Log in</p>
 
 
             </button>
 
-            <button className='mt-2 absolute md:right-[80px] flex rounded-full p-[2px] w-[125px] h-[50px] px-8 py-3 border-2 border-[#FF4967]'>
+            <button className='mt-2 absolute md:right-[80px] right-[160px] flex rounded-full p-[2px] md:w-[125px] h-[50px] px-8 py-3 border-2 border-[#FF4967] md:block hidden'>
             <p className=' text-[#FF4967] text-center mx-auto font-medium'>Sign up</p>
             
             </button>
@@ -32,7 +33,7 @@ function HomeNav() {
       
     </div>
 
-    <hr className='border-b border-black pt-32 flex items-center justify-center w-4/5 mx-auto' />
+    
     </div>
   )
 }
--- src/components/home/Homehero.jsx
+++ src/components/home/Homehero.jsx
@@ -3,18 +3,25 @@ import React from 'react'
 function Homehero() {
   return (
     <div className=' '>
-        <div className=' md:grid-cols-2 grid overflow-hidden'>
+        <div className=' md:grid-cols-2 grid md:h-[100vh]'>
         <div className=' md:pt-20 lg:ml-28 '>
-            <h1 className=' lg:text-7xl text-6xl leading-snug font-semibold hidden md:block '>Moodly<span className='text-[#549CE1]'>.</span></h1>
+            <h1 className=' lg:text-7xl text-6xl leading-snug font-semibold hidden md:block mt-20 '>Moodly<span className='text-[#549CE1]'>.</span></h1>
 
-            <p className='w-[40%] md:text-xl text-3xl mt-10 md:text-gray-700 text-black font-bold mx-2 text-center'>Track your mood with Moodly</p>
-            <p className='md:mt-14 mt-10 md:text-xl text-2xl text-gray-400 text-center  w-[400px] '>Moodly is a free to use and daily mood tracker </p>
+            <p className='md:text-xl text-4xl md:mt-10 mt-5 md:text-gray-700 text-black font-bold mx-2 text-center md:text-left'>Track your mood with Moodly</p>
+            <p className='md:mt-14 mt-10 md:text-xl text-2xl text-gray-400 text-center md:text-left  '>Moodly is a free and easy to use daily mood tracker </p>
 
-            <div className='flex justify-center items-center w-[400px] mt-10'>
+            <div className='flex justify-center items-center  mt-10 gap-5'>
 
-            <button className='mt-20 absolute   bg-[#FF4967] rounded-full p-[2px] w-[180px] h-[70px] px-8 py-3' >
+            <button className='  bg-[#FF4967] rounded-full p-[2px] md:w-[180px] w-[150px] h-[70px] px-8 py-3' >
 
-<p className=' text-white text-center mx-auto font-medium'>Get started</p>
+<p className=' text-white text-center mx-auto font-medium'>Instructor</p>
+
+
+</button>
+
+<button className='    bg-white text-[#FF4967] border-2 border-[#FF4967] rounded-full p-[2px] md:w-[180px] w-[150px] h-[70px] px-8 py-3' >
+
+<p className=' text-center mx-auto font-medium'>Student</p>
 
 
 </button>
@@ -22,13 +29,13 @@ function Homehero() {
 </div>
         </div>
 
-        <div className=' order-first'>
+        <div className=' order-first md:order-last overflow-hidden md:mt-1 mt-10'>
 
-            <div className=' md:w-[1200px] w-[900px] h-full'>
+            <div className=' md:w-[1000px] w-[500px] -ml-16  '>
                 <img
-                src={"/mood1.png"}
+                src={"/mood4.png"}
                 alt='contacts1'
-                className='md:ml-40 md:w-[850px] w-[750px] '
+                className='md:w-[750px]  '
                 />
             </div>
             
