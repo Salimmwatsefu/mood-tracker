b076ee8a4c6f28df2e4d2b54f6e90afb1c388b39
Mohameddek1 <mohadek2004@com>
2024-03-18 22:29:45 +0300

    From new branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import InstructorLogin from './components/Authentication/InstructorLogin';
 import InstructorSignup from './components/Authentication/InstructorSignup';
 import StudentLogin from './components/Authentication/StudentLogin';
 import StudentSignup from './components/Authentication/StudentSignup';
+import ChooseMood from './components/Student/Mood/ChooseMood'
+import Intensity from './components/Student/Mood/Intensity';
+import Student from './components/Student/studentPage';
 import Home from './components/home/Home';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
@@ -12,16 +15,17 @@ function App() {
 
   return (
     <>
-
-
     <Routes>
       <Route index path='/' element={<Home />} />
       <Route path='/student-login' element={<StudentLogin />} />
       <Route path='/student-signup' element={<StudentSignup />} />
       <Route path='/instructor-login' element={<InstructorLogin />} />
       <Route path='/instructor-signup' element={<InstructorSignup />} />
+      <Route path='/choose-mood' element={<ChooseMood />} />
+      <Route path='/choose-mood/intensity' element={<Intensity />} />
+      <Route path='/student' element={<Student />} />
+
     </Routes>
-      
     </>
   )
 }
