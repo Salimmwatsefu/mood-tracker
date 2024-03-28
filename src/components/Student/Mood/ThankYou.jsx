fe7515739f0c306fc06bc30c276ca782de20e3f9
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2024-03-28 11:32:00 +0300

    add thank you page

--- src/components/Student/Mood/ThankYou.jsx
+++ src/components/Student/Mood/ThankYou.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function ThankYou() {
   return (
-    <div>ThankYou</div>
+    <div className='mx-auto sm:w-[50%] w-[380px] '>
+    <section class="rounded-3xl shadow-2xl md:mt-32 mt-52">
+  <div class="p-8 text-center sm:p-12">
+    <p class="text-sm font-semibold uppercase tracking-widest text-pink-500">
+      Good Work
+    </p>
+
+    <div className=' flex justify-center'>
+      <div className=' w-52'>
+        <img src="/logom.png" alt="Logo" />
+      </div>
+    </div>
+
+    <h2 class="mt-6 text-3xl font-bold">Thanks for your Feedback</h2>
+
+   
+  </div>
+</section>
+    </div>
   )
 }
 
